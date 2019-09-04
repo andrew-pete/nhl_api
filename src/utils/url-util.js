04332d5605ec3af432503e32a57d21c04565cad3
Andrew Peterson <alp98@cornell.edu>
2019-09-03 21:50:03 -0700

    feature: shifts/:id endpoint functional

--- src/utils/url-util.js
+++ src/utils/url-util.js
@@ -1,11 +1,24 @@
 function parseId(fullId) {
-    const year = fullId.substring(0, 8);
-    const gameId = fullId.substring(8);
+    const year = fullId.substring(0, 4);
+    const gameId = fullId.substring(4);
 
     return { year, gameId };
 }
 
+function expandYear(year) {
+    if (year.length === 8) {
+        return year;
+    } else if (year.length === 4 && !isNaN(+year)) {
+        const yearStart = +year;
+        const yearPlusOne = yearStart + 1;
+        return `${yearStart}${yearPlusOne}`;
+    } else {
+        throw `Invalid year: ${year}`;
+    }
+}
+
 module.exports = {
     parseId,
+    expandYear
 };
 
